8b4defc37a61c45cd77374ce7c6bf0373cf530be
xiaobai <1300940071@qq.com>
2020-03-29 20:25:02 +0800

    富文本编辑器

--- src/main/java/com/yy/controller/PageController.java
+++ src/main/java/com/yy/controller/PageController.java
@@ -1,6 +1,7 @@
 package com.yy.controller;
 
 import com.yy.pojo.Commodity;
+import com.yy.service.CommodityService;
 import com.yy.service.PageService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,13 +19,18 @@ public class PageController {
 
     @Resource
     private PageService pageService;
+    @Resource
+    private CommodityService commodityService;
 
     @RequestMapping("/getCommodity")
-    public String getCommodity(int id, Model model){
+    public String getCommodity(Commodity commodity, Model model){
         //根据分类查询出编辑精选的商品
-       List<Commodity> jinxuan=pageService.getListbyCategoryIdAndreecom(id);
+       //List<Commodity> jinxuan=pageService.getListbyCategoryIdAndreecom(id);
+        List<Commodity> jinxuan=commodityService.selectByCom(commodity);
         //根据分类查询出最新的商品
-       List<Commodity> zuixin=pageService.getListByTime(id);
-        return "commodity";
+       //List<Commodity> zuixin=pageService.getListByTime(id);
+        List<Commodity> zuixin=commodityService.selectByCom(commodity);
+        System.out.println("1111");
+        return "/page/commodity";
     }
 }
