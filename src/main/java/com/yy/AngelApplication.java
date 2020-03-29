1845d9d0140fc5c6efac275b88c296aff6d919a2
xiaobai <1300940071@qq.com>
2020-03-29 19:44:47 +0800

    富文本编辑器

--- src/main/java/com/yy/AngelApplication.java
+++ src/main/java/com/yy/AngelApplication.java
@@ -1,9 +1,13 @@
 package com.yy;
+import com.github.pagehelper.PageHelper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Properties;
 
 
 @SpringBootApplication
@@ -14,6 +18,19 @@ public class AngelApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AngelApplication.class, args);
+    }
+
+    //配置mybatis的分页插件pageHelper
+    @Bean
+    public PageHelper pageHelper(){
+        PageHelper pageHelper = new PageHelper();
+        Properties properties = new Properties();
+        properties.setProperty("offsetAsPageNum","true");
+        properties.setProperty("rowBoundsWithCount","true");
+        properties.setProperty("reasonable","true");
+        properties.setProperty("dialect","mysql");    //配置mysql数据库的方言
+        pageHelper.setProperties(properties);
+        return pageHelper;
     }
 
 }
--- src/main/java/com/yy/controller/CommodityController.java
+++ src/main/java/com/yy/controller/CommodityController.java
@@ -2,11 +2,13 @@ package com.yy.controller;
 
 
 import com.alibaba.fastjson.JSON;
+import com.github.pagehelper.PageHelper;
 import com.yy.pojo.Commodity;
 import com.yy.pojo.ResultTable;
 import com.yy.service.CommodityService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
@@ -27,7 +29,11 @@ public class CommodityController {
 
     @RequestMapping("/getCommodityList")
     @ResponseBody
-    public String getList(ResultTable resultTable){
+    public String getList(ResultTable resultTable,@RequestParam(value="pn",defaultValue="1") Integer pn,Commodity commodity){
+        //获取第1页，5条内容，默认查询总数count
+        /* 第一个参数是第几页；第二个参数是每页显示条数 */
+        PageHelper.startPage(pn, 8);
+
         List<Commodity> commodityList=commodityService.selectByCom(null);
         resultTable.setCode(0);
         resultTable.setData(commodityList);
